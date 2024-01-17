2eb2f0e31f3f3556b4c457d9e069817c5a5df318
Ayman <mohammedaymanquadri@gmail.com>
2024-01-17 19:33:51 +0530

    Basic structure added

---
+++ script.js
@@ -0,0 +1,11 @@
+const pictures = [
+    'https://static.vecteezy.com/system/resources/thumbnails/016/716/602/small/shoot-gun-3d-rendeirng-png.png',
+    'https://freepngimg.com/thumb/football/36635-3-football-soccer-ball-thumb.png',
+    'https://static.vecteezy.com/system/resources/thumbnails/010/521/120/small/basketball-sport-balloon-free-vector.jpg',
+    'https://static.vecteezy.com/system/resources/thumbnails/026/971/084/small/biryani-or-biryani-is-a-popular-indian-dish-made-of-basmati-rice-cooked-with-spices-and-vegetables-free-photo.jpg',
+    'https://static1.personality-database.com/profile_images/fb6997d4b3c74ac6bca1550d0f067d3e.png',
+    'https://imgs2.dab3games.com/among-us-2-online355.png'
+]
+
+
+
